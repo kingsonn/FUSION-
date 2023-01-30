8ccc99a81960e031f2b28f2baf7df2952088fdbd
kingsonn <hansonnnnnnn@gmail.com>
2023-01-30 16:50:54 +0530

    Footer

--- client/src/App.js
+++ client/src/App.js
@@ -6,40 +6,10 @@ import { useStateValue } from "./context/StateProvider";
 import { getAllFoodItems } from "./utils/firebaseFunctions";
 import { actionType } from "./context/reducer";
 import { Link } from "react-router-dom";
-
-function Footer() {
-
-  const gmailHandler = () => {
-    window.open(
-      "mailto:" +
-      "hansonnnnnnn@gmail.com" +
-      "?subject=" +
-      " " +
-      "&body=" +
-      " ",
-      "_self"
-    );
-  };
-  return (
-    <div className="bg-gradient-to-br from-orange-400 to-orange-500 py-8 px-6 text-black  lg:text-base text-sm">
-      <div className="max-w-screen-xl w-full mx-auto">
-        <div className="flex justify-between items-center">
-          <div  className="flex items-center lg:space-x-8 space-x-4" >
-          
-              <span onClick={gmailHandler} className=" test-black cursor-pointer hover:text-black">Email</span>
-                <a href="https://github.com/kingsonn" className="cursor-pointer hover:text-black">Github</a>
-          </div>
-                
-          
-        </div>
-        <p className="mt-6 text-black text-center flex items-center flex-wrap justify-center">
-        © 2023, Built by<span className="text-black hover:underline ml-2">Hanson Braganza
-          </span>
-        </p>
-      </div>
-    </div>
-  );
-}
+import {store} from "./utils/store";
+import StorageService from "./utils/StorageService";
+import { hydrate} from "./utils/cartSlice"
+import Footer from "./components/Footer";
 
 const App = () => {
   const [{ foodItems }, dispatch] = useStateValue();
@@ -52,7 +22,14 @@ const App = () => {
       });
     });
   };
-
+  useEffect(() => {
+    store.subscribe(() => {
+        StorageService.set("cart", JSON.stringify(store.getState().cart));
+    });
+    let cart = StorageService.get("cart");
+    cart = cart ? JSON.parse(cart) : { items: [] };
+    store.dispatch(hydrate(cart));
+}, []);
   useEffect(() => {
     fetchData();
   }, []);
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+function Footer() {
+
+    const gmailHandler = () => {
+      window.open(
+        "mailto:" +
+        "hansonnnnnnn@gmail.com" +
+        "?subject=" +
+        " " +
+        "&body=" +
+        " ",
+        "_self"
+      );
+    };
+    return (
+      <div className="bg-gradient-to-br from-orange-400 to-orange-500 py-8 px-6 text-black  lg:text-base text-sm">
+        <div className="max-w-screen-xl w-full mx-auto">
+          <div className="flex justify-between items-center">
+            <div  className="flex items-center lg:space-x-8 space-x-4" >
+            
+                <span onClick={gmailHandler} className=" test-black cursor-pointer hover:text-black">Email</span>
+                  <a href="https://github.com/kingsonn" className="cursor-pointer hover:text-black">Github</a>
+            </div>
+                  
+            
+          </div>
+          <p className="mt-6 text-black text-center flex items-center flex-wrap justify-center">
+          © 2023, Built by<span className="text-black hover:underline ml-2">Hanson Braganza
+            </span>
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+  export default Footer
