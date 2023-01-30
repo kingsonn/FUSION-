7168acd1ad1722982fc4bb1426574fdeb4626791
kingsonn <hansonnnnnnn@gmail.com>
2023-01-30 16:50:22 +0530

    UI changes

--- client/src/components/HomeContainer.jsx
+++ client/src/components/HomeContainer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import Fade from "react-reveal/Fade";
+import circle from "../img/circle.svg"
 const HomeContainer = () => {
   return (
     <section
@@ -7,14 +8,25 @@ const HomeContainer = () => {
       id="home"
     >
       <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
-     
+      <div style={{zIndex:-0}} className="absolute lg:-bottom-60 -bottom-72 lg:-left-44 -left-80  object-contain overflow-hidden">
+        <Fade left>
+        <img src={circle} alt="" width={400} height={400} />
+        </Fade>
+      </div>
 
-        <p className="text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor">
-          Are you Hungry?
+      <div style={{zIndex:-0}} className="absolute top-16 lg:left-72 left-60 lg:w-auto sm:w-10 w-8 object-contain overflow-hidden">
+        <Fade top>
+        <img src={circle} alt="" width={80} height={80} />
+        </Fade>
+      </div>
+
+        <div  className="text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor">
+          Are you 
+          <p>Hungry?</p>
           <p className="text-orange-600 text-[3rem] lg:text-[5rem]">
             Dont Wait!
           </p>
-        </p>
+        </div>
 
         <p className="text-base text-textColor text-center md:text-left md:w-[80%]">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima velit
@@ -24,6 +36,7 @@ const HomeContainer = () => {
         </p>
 
         <button
+        style={{zIndex:3}}
           type="button"
           className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2  rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
         >
